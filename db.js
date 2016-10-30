b90952b36b2848344f674f494c47dd371fd00b30
chaegumi <chaegumi@qq.com>
2016-10-30 12:03:13 +0800

    Change dbfile path and packager

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
+const path = require('path')
+const dbPath = path.resolve(__dirname, 'data/test.db.sqlite')
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(`data/test.db.sqlite`, sqlite3.OPEN_READWRITE);
+var db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE);
--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow(){
 
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
 	mainWindow.on('closed', function(){
 		mainWindow = null;
